8b60b69da0a07d36e27047d6b06a6539446cc38d
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-08-14 04:57:34 -0400

    Initial Commit

---
+++ TestSpriteKitScene/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSpriteKitScene
+//
+//  Created by 齋藤緒 on 2016/08/14.
+//  Copyright © 2016年 TestOrganization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
